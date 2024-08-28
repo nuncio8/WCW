4915bca9ab48156d7da20a48e2b309c227de3934
Francisco Nuncio~ <francisco.nuncio@tecnico.ulisboa.pt>
2024-08-28 20:07:37 +0100

    header correction

---
+++ script_file.js
@@ -0,0 +1,12 @@
+// When the user clicks on div, open the popup
+function myFunction() {
+var popup = document.getElementById("myPopup");
+popup.classList.toggle("show");
+}
+
+
+//tips for trying animation. 
+//to move the element use "document.getElementById("changing_quote").innerText = quote_list[quote_i]"
+
+//changing quote code
+
